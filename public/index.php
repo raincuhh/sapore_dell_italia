8e9eeb4e3f5fbf8dc24cd9517574b56dd8332328
raincuhh <146904648+raincuhh@users.noreply.github.com>
2024-10-28 13:24:11 +0100

    initial commit

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <meta name="description" content="sum" />
+   <link rel="preconnect" href="https://fonts.googleapis.com" />
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+   <title>Test</title>
+</head>
+
+<body>
+   <noscript>
+      <div id="n_script_c">
+         <div id="n_script_h">Please enable JavaScript to view this website.</div>
+      </div>
+   </noscript>
+   <script nomodule>
+      let d = document;
+
+      let cont = d.createElement("div");
+      cont.id = "n_script_c";
+      d.body.appendChild(cont);
+
+      let header = d.createElement("div");
+      header.id = "n_script_h";
+      header.textContent = "Please enable JavaScript to view this website.";
+      cont.appendChild(header);
+   </script>
+   <div id="root"></div>
+</body>
+
+</html>
